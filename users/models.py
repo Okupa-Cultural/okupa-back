39000be7aec56287f40e0c80deccf14e7fd68ff3
Chicho <lgarcialuzzi@gmail.com>
2019-12-06 23:10:12 -0300

    agregado superusuario

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    mail = models.EmailField()
+    start_date = models.DateField()
+    bio = models.TextField(null=True, blank=True)
